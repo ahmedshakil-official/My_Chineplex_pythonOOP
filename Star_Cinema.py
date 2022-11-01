d0c8a464b0f949863cf6d6c23111dbc69dddadf8
Shakil <webapplication.shakil@gmail.com>
2022-11-02 00:39:39 +0600

    Abstraction added

--- Star_Cinema.py
+++ Star_Cinema.py
@@ -1,38 +1,39 @@
-class Star_Cinema:
-    hall_list = []
+class _Star_Cinema:
+    __hall_list = []
 
     def __init__(self, rows, cols, hall_no):
-        self.rows = rows
-        self.cols = cols
-        self.hall_no = hall_no
+        self._rows = rows
+        self._cols = cols
+        self._hall_no = hall_no
 
     def entry_hall(self):
-        hall = [self.hall_no, self.rows, self.cols]
-        Star_Cinema.hall_list.append(hall)
+        hall = [self._hall_no, self._rows, self._cols]
+        _Star_Cinema.__hall_list.append(hall)
 
 
-class Hall(Star_Cinema):
+class _Hall(_Star_Cinema):
     def __init__(self, rows, cols, hall_no):
-        self.seats = {}
-        self.show_list = []
+        self.__seats = {}
+        self.__show_list = []
         super().__init__(rows, cols, hall_no)
 
     def entry_show(self, id, movie_name, time):
         show = (id, movie_name, time)
         lst = []
-        self.show_list.append(show)
-        for i in range(self.rows):
-            r = [f'{chr(i + 65)}{j + 1}' for j in range(self.cols)]
+        self.__show_list.append(show)
+        for i in range(self._rows):
+            r = [f'{chr(i + 65)}{j + 1}' for j in range(self._cols)]
             lst.append(r)
-        self.seats[id] = lst
+        self.__seats[id] = lst
 
     def book_seats(self, name, phone_number, id, book_seats):
         correct_id = False
         booked = False
-        for key, value in self.seats.items():
+        for key, value in self.__seats.items():
             if key == id:
                 correct_id = True
                 for seat in book_seats:
+
                     if value[seat[0] - 1][seat[1] - 1] != 'X':
                         value[seat[0] - 1][seat[1] - 1] = 'X'
                     else:
@@ -48,55 +49,96 @@ class Hall(Star_Cinema):
         movie_name = ""
         time = ""
         b_seats = ""
-        for show in self.show_list:
+        for show in self.__show_list:
             if show[0] == id:
                 movie_name = show[1]
                 time = show[2]
 
         for s in book_seats:
-            b_seats += f'{chr(s[0]-1+65)}{s[1]} '
+            b_seats += f'{chr(s[0] - 1 + 65)}{s[1]} '
 
         if not booked and correct_id:
-            print("--------------------------------------------------------")
+            print("-----------------------------------------------------------------------------------")
             print("######### Booking ticket successful! #########")
-            print("--------------------------------------------------------")
+            print("-----------------------------------------------------------------------------------")
             print(f'Name: {name}')
             print(f'Phone Number: {phone_number}\n\n')
             print(f'Movie Name: {movie_name}\t\t\tMovie Time: {time}')
-            print(f'Tickets: {b_seats}\t\t\tHall: {self.hall_no}')
-            print("--------------------------------------------------------")
+            print(f'Tickets: {b_seats}\nHall: {self._hall_no}')
+            print("-----------------------------------------------------------------------------------")
 
     def view_show_list(self):
-        for show in self.show_list:
+        for show in self.__show_list:
             print(f'Movie Name: {show[1]}\t\t\tShow ID: {show[0]}\t\t\tTime: {show[2]}')
 
     def view_available_seats(self, id):
 
-        for key, value in self.seats.items():
+        valid_id = False
+        for key, value in self.__seats.items():
             if key == id:
-                for i in range(self.rows):
-                    for j in range(self.cols):
+                valid_id = True
+                for i in range(self._rows):
+                    for j in range(self._cols):
                         if value[i][j] != 'X':
                             print(value[i][j], end=" ")
                     print()
-
-    def menu(self):
-        print("------------------------------------------------")
-        print("1. View all running shows.")
-        print("2. View available seats.")
-        print("3. Book tickets.")
+        if not valid_id:
+            print("This movie id is wrong or unavailable. Please try again!")
+            print("-----------------------------------------------------------------------------------")
 
 
-h = Hall(5, 7, "a22")
-h2 = Hall(5, 7, "a33")
-h.entry_show("www", "Bostir Rani Suriya", "2.06pm")
+def menu():
+    print("-----------------------------------------------------------------------------------")
+    print("1. View all running shows.")
+    print("2. View available seats.")
+    print("3. Book tickets.")
+    print("4. Exit.")
+
+
+h = _Hall(5, 22, "a22")
+
+h2 = _Hall(5, 7, "a33")
+h.entry_show("www", "Bostir Rani Suriya", "2.00pm")
 h.entry_show("xxx", "No 1 Shakib Khan", "7.30pm")
 h.entry_show("zzz", "Gorib The Chotolok", "9.30pm")
-h.entry_show("aaa", "Sokal The Morning", "1.30pm")
+h.entry_show("aaa", "Sokal The Morning", "12.30pm")
 h.entry_hall()
 h2.entry_hall()
-h.book_seats("Shakil", "01*******", "www", [(1, 3), (1, 1)])
-h.book_seats("Soha", "01*******", "aaa", [(1, 3), (2, 4)])
-# h.view_show_list()
-h.view_available_seats("www")
-h.view_show_list()
+run = True
+while run:
+    menu()
+    x = int(input("Enter your choice: "))
+    if x == 1:
+        print("-----------------------------------------------------------------------------------")
+        h.view_show_list()
+    elif x == 2:
+        id = input("Enter movie ID: ")
+        print("-----------------------------------------------------------------------------------")
+        h.view_available_seats(id)
+    elif x == 3:
+        name = input("Enter your name: ")
+        phone_number = input("Enter your mobile number: ")
+        id = input("Enter movie ID: ")
+        number_of_tickets = int(input("Enter number of tickets you want: "))
+        seats = input("Enter seat number: ")
+        lst = seats.split(" ")
+        ticket = []
+        is_ok = True
+        for i in lst:
+            t = (ord(i[0]) - 65 + 1, int(i[1:]))
+            ticket.append(t)
+            if t[0] < 1 or t[0] >= h._rows or t[1] < 1 or t[1] >= h._cols:
+                is_ok = False
+                break
+        if is_ok:
+            h.book_seats(name, phone_number, id, ticket)
+        else:
+            print("-----------------------------------------------------------------------------------")
+            print("######### Sorry!!! Invalid seat number. #########")
+            print("-----------------------------------------------------------------------------------")
+    elif x == 4:
+        run = False
+    else:
+        print("Wrong input. Try again")
+
+
